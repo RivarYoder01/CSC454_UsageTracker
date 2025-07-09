c13056a77a3738de20455cd9f06c04fc7dd57bcf
RivarYoder01 <alyssa.stars.101@gmail.com>
2025-07-08 20:00:50 -0500

    wrote basic display_led. Just an oval filled in with gray. This will act as a virtual LED that will toggle between red, yellow, and green. SOURCES: https://www.geeksforgeeks.org/python/python-tkinter-create-different-shapes-using-canvas-class/

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ CPU Usage LED Simulator
 """
 
 import psutil
-import time
+import tkinter as tk
 
 # CPU Usage LED Simulator
 # Using psutil, simulate a CPU monitor that:
@@ -12,14 +12,33 @@ import time
 # • Color/brightness changes based on CPU %
 # • Alert system if CPU > 90%
 
+root = None
+canvas = None
+led = None
+
+def display_led():
+    """
+
+    :return:
+    """
+    global root, canvas, led
+    root = tk.Tk()
+    root.title("CPU LED MONITOR")
+
+    canvas = tk.Canvas(root, width=80, height=80)
+    canvas.pack()
+
+    led = canvas.create_oval(10, 10, 70, 70, fill="gray", outline="black", width=2)
+
 
 def main():
     """
     """
-    while True:
-        cpu_percent = psutil.cpu_percent(interval=1)
-        print(f"Current CPU usage: {cpu_percent}%")
-        time.sleep(1)  # Wait for 1 second before checking again
+    display_led()
+    root.mainloop()
+    # while True:
+        # cpu_percent = psutil.cpu_percent(interval=1)
+        # print(f"Current CPU usage: {cpu_percent}%")
 
 if __name__ == "__main__":
     main()
